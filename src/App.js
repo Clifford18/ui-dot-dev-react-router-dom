3afcb324b52a647d3cc2e53db031e624e9794442
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-02 15:58:50 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 
 import './App.css';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import ErrorPage from "./Pages/ErrorPage";
+import Messages from "./Pages/Messages";
+import Settings from "./Pages/Settings";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    return (
+        <Router>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about" >About </Link>
+                <Link to="/profile" >Profile </Link>
+                <Link to="/messages" >messages</Link>
+                <Link to="/settings" >settings</Link>
 
-      </header>
-    </div>
-  );
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile/:username" element={<Profile/>} />
+                <Route path="*" element={<ErrorPage/>} />
+                <Route path="/messages" element={<Messages />} />
+                <Route path="/settings" element={<Settings />} />
+
+
+            </Routes>
+        </Router>
+
+    );
 }
 
 export default App;
